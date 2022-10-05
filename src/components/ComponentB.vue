860a08c4b30c87931fd552bb0b80d85b76ff75ce
maingocthanhtan96 <maingocthanhtan96@gmail.com>
2022-10-05 22:11:43 +0700

    Init commit

---
+++ src/components/ComponentB.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+.counter {
+  label {
+    padding-right: 10px;
+    font-weight: bold;
+  }
+  .count {
+  padding: 0 10px;
+}
+}
+</style>
